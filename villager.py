fe4c5ec4acdd4e724b5150527e73eb6e07a85fbc
Megha Murthy <67402270+memu1227@users.noreply.github.com>
2026-01-26 23:33:44 -0500

    adding some analysis

--- villager.py
+++ villager.py
@@ -5,3 +5,20 @@ import matplotlib.pyplot as plt
 #import the villager file
 data = pd.read_csv("villagers.csv")
 #print(data)
+
+#look at the contents of the data 
+#print(data.info) 
+#no null values
+
+#Find the number of distinct species
+species = data['Species'].value_counts()
+
+#create a bar chart of the most frequent species
+species.plot(kind='bar',color= 'purple')
+plt.xlabel('Species')
+plt.ylabel('Count')
+plt.title('Frequency of Villagers by Species')
+plt.yticks(range(0, int(species.max()) + 1, 1)) 
+plt.grid(alpha=0.3)  
+plt.tight_layout()
+plt.show()
